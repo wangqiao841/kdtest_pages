432b5f1ee05ee2e34327847f7e59118c4a3ced9d
WangQiao <1603938216@qq.com>
2024-01-25 19:17:27 +0800

    代码展示区，复制按钮支持粘性定位

--- javaScript/cp.js
+++ javaScript/cp.js
@@ -1,26 +1,32 @@
 $(function() {
-	$.fn.copy_init = function(){
+	$.fn.copy_init = function(copy_text_array){
 		var pres = $("pre");
+		var index = 0;
 		// console.log(pres.length)
 		if (pres.length) {
 			pres.each(function() {
 				var that = this;
-				var t = $(that).find("code").text();
-				var btn = $('<span class="copy el-icon-copy-document" title="复制"></span>').attr(
+				// var copy_text = $(that).find("code").text();
+				var btn = $('<span class="copy" title="复制">复制</span>').attr(
 					"data-clipboard-text",
-					t
+					copy_text_array[index]
 				);
 				$(that).prepend(btn);
+				// that.insertAdjacentHTML('afterbegin', btn)
 				var c = new ClipboardJS(btn[0]);
 				c.on("success", function() {
-					btn.addClass("copyed").text("  复制成功");
+					btn.css("background-color", "#34bd3d");
+					btn.text("已复制");
 				});
 				c.on("error", function() {
-					btn.text("  复制失败");
+					btn.css("background-color", "#bd3638");
+					btn.text("失败");
 				});
 				btn.mouseleave(function() {
-					btn.text("").removeClass("copyed");
+					btn.css("background-color", "#adadad");
+					btn.text("复制");
 				});
+				index += 1;
 			});
 		}
 	}
